67d34e315c8284974532c11818b39e3fad7026b5
Everton Martins <emartinssjc@gmail.com>
2026-08-24 19:34:28 -0300

    Add files via upload

--- pages/1_Visao_Empresa.py
+++ pages/1_Visao_Empresa.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import folium
 from streamlit_folium import folium_static
 st.set_page_config(page_title = 'Visão Empresa', page_icon='XD', layout='wide')
-df = pd.read_csv('train.csv')
+df = pd.read_csv('../dataset/train.csv')
 
 
 df1 = df.copy()

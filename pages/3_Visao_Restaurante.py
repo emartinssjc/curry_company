5669f06f1a6d88ca513315905110cf035ccdddd3
Everton Martins <emartinssjc@gmail.com>
2026-08-24 20:05:50 -0300

    Update 3_Visao_Restaurante.py

--- pages/3_Visao_Restaurante.py
+++ pages/3_Visao_Restaurante.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import folium
 from streamlit_folium import folium_static
 st.set_page_config(page_title = 'Visão Restaurantes', page_icon='XD', layout='wide')
-df = pd.read_csv('../dataset/train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 
 df1 = df.copy()
